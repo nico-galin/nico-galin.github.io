f58c141459826733993ce952dc751d7f10de6153
Nico Galin <nico.galin@gmail.com>
2022-01-14 16:06:26 -0800

    Completed simple resume page"

---
+++ src/data/resume.js
@@ -0,0 +1,168 @@
+const resume = {
+    "experience": [
+        {
+            "title": "ECAD Software Intern",
+            "company": "Apple",
+            "start": "Jan 2021",
+            "end": "Aug 2021",
+            "paren": "8 MO",
+            "descriptions": [
+                "Developed and documented PCB design and validation utilities in frontend frameworks for internal use in an Agile development environment."
+            ]
+        },
+        {
+            "title": "External Vice President (VP Engineering)",
+            "company": "Web Development at Berkeley",
+            "start": "Dec 2021",
+            "end": "Present",
+            "descriptions": [
+                "Working on client acquisition and outreach to Berkeley-founded companies and student organizations",
+                "Implementing organization-wide development conventions and guiding engineering culture",
+                "Overseeing product managers and developers on industry projects"
+            ]
+        },
+        {
+            "title": "Frontend Developer",
+            "company": "Web Development at Berkeley",
+            "start": "Sep 2021",
+            "end": "Dec 2021",
+            "paren": "4 MO",
+            "descriptions": [
+                "Developing an analytics dashboard for Provecho in React.js, SCSS, and Firebase on a team of 8 using the Agile/SCRUM project management framework.",
+            ]
+        },
+        {
+            "title": "Associate Product Manager",
+            "company": "sproul.club",
+            "start": "Jan 2021",
+            "end": "Sep 2021",
+            "paren": "9 MO",
+            "descriptions": [
+                "Managing 5 team-members across Product Design, Backend and Frontend Development, and User Testing sectors developing the sproul.club student account features.",
+                "Served as a SCRUM Master for following the Agile workflow with JIRA.",
+            ]
+        },
+        {
+            "title": "Frontend Developer",
+            "company": "sproul.club",
+            "start": "Oct 2020",
+            "end": "Jan 2021",
+            "paren": "4 MO",
+            "descriptions": [
+                "Web development in ReactJS, Redux, and Flask for sproul.club, a platform providing Berkeley students access to campus organizations and clubs.",
+            ]
+        },
+        {
+            "title": "Software Engineering Intern",
+            "company": "SummerBio.",
+            "start": "May 2020",
+            "end": "Aug 2020",
+            "paren": "4 MO",
+            "descriptions": [
+                "Designed and implemented a mobile cross-platform app in Capacitor/AngularJS, HTML, SCSS, and Adobe XD for personal COVID-19 test submission and expedited test result delivery.",
+                "Tested and modified multiple contact tracing applications implementing Google/Apple’s API and the TCN Bluetooth Protocol for internal use.",
+                "Published COVID-19 test management app to beta testers on iOS and Android.",
+                "Developed NodeJS backend for both contact tracing and test result applications through Google Firebase and Amazon Web Services.",
+            ]
+        },
+        {
+            "title": "Software Engineering Intern",
+            "company": "Joby Aviation",
+            "start": "Jun 2018",
+            "end": "May 2020",
+            "paren": "2 YR",
+            "descriptions": [
+                "Developed a mobile cross-platform flight booking application using Apache Cordova (and later) React Native frameworks, HTML, and CSS on a small team.",
+                "Implemented backend functionality with Amazon Web Services and REST APIs for syncing, automated scheduling, and updating.",
+                "Published flight booking app on internal test tracks for both iOS and Android devices.",
+                "Coordinated with a team of 6-8 in an Agile/SCRUM environment.",
+            ]
+        },
+        {
+            "title": "Software Development Consultant",
+            "company": "GammaDynamics",
+            "start": "Sep 2016",
+            "end": "Jan 2018",
+            "paren": "1 YR 5 MO",
+            "descriptions": [
+                "Developed a cross-platform web application in Apache Cordova, HTML, and CSS to provide access to a trained neural network on product reviews.",
+                "Published multiple apps on the Apple and Google App Stores.",
+            ]
+        },
+    ],
+    "education": [
+        {
+            "school": "University of California, Berkeley",
+            "degrees": [
+                "B.A. Computer Science",
+                "B.A. Data Science (Cognition)"
+            ],
+            "gpa": "3.76"
+        }
+    ],
+    "projects": [
+        {
+            "title": "Crypto Mining",
+            "years": ["]2021"],
+            "description": "Set up Ethereum (ETH) and Monero (XMR) mining rigs using custom built desktop PCs running Windows and HiveOS."
+        },
+        {
+            "title": "Lyftable",
+            "years": ["2021"],
+            "description": "Progress tracking social media platform for sharing and comparing fitness improvement with friends and followers. Built using Adobe XD, React Native, and Google Firebase."
+        },
+        {
+            "title": "Personal Site",
+            "years": ["2020", "2021"],
+            "description": "Github-hosted personal site built with React.js and SCSS."
+        },
+        {
+            "title": "NiqBot",
+            "years": ["2020"],
+            "description": "DIscord bot that tracks user disconnect times and provides analytics (graphs, means, etc.) with bedtime predictions and server bedtime leaderboards."
+        },
+        {
+            "title": "Sentiment Analysis",
+            "years": ["2019"],
+            "description": "Machine learning algorithm to determine the general sentiment (Positive, Neutral, Negative) of text using TensorFlow and Python3."
+        },
+        {
+            "title": "NumC",
+            "years": ["2020"],
+            "description": "Custom NumPy distribution with optimized matrix and float operations."
+        },
+    ],
+    "skills": {
+        "languages": "Python, Java,Script, Java, C, RISC-V, HTML, CSS, SCSS",
+        "technologies": "React.js, React Native, Angular, Apache Cordova, Capacitor, REST, Node.js, Git",
+        "project management": "JIRA, Confluence, SCRUM, Agile, Asuna, Lucid Chart",
+    },
+    "volunteer": [
+        {
+            "title": "Web Design Mentor",
+            "company": "Cubstart Web Design DeCal",
+            "start": "Sep 2020",
+            "end": "May 2021",
+            "paren": "9 MO",
+            "description": "Mentoring React.js, HTML, and CSS for an introductory web design course at Berkeley.",
+        },
+        {
+            "title": "Internal/External Affairs Committee Member",
+            "company": "Greening the Greeks",
+            "start": "Jan 2021",
+            "end": "Present",
+            "paren": "",
+            "description": "",
+        },
+        {
+            "title": "Web Dev Project Mentor",
+            "company": "Intro to Fullstack Dev",
+            "start": "Nov 2021",
+            "end": "Dec 2021",
+            "paren": "1MO",
+            "description": "",
+        },
+    ]
+}
+
+export default resume;
--- src/pages/Resume/Resume.js
+++ src/pages/Resume/Resume.js
@@ -1,13 +1,113 @@
-import './Resume.module.scss';
+import styles from './Resume.module.scss';
 import React from 'react';
-import logo from '../../logo.svg';
+import { notBlank } from '../../utils';
+import resume from '../../data/resume';
+import ResumeSection from '../../components/ResumeSection/ResumeSection';
+
+let experienceRaw = {};
+resume.experience.forEach((exp, ind) => {
+    if (!experienceRaw[exp.company]) {
+        experienceRaw[exp.company] = {
+            index: ind,
+            data: []
+        }
+    }
+    experienceRaw[exp.company].data.push({...exp})
+})
+
+let experience = [];
+Object.keys(experienceRaw).forEach((key) => {
+    experience.splice(experienceRaw[key].index, 0, experienceRaw[key].data)
+})
+
+const getSubtitles = (data) => {
+    let ret = [];
+    data.forEach(el => {
+        ret.push(`${el.title} / ${el.start} - ${el.end}${notBlank(el.paren) ? " (" + el.paren + ")" : null}`);
+    })
+    return ret;
+}
+
+const getDescriptions = (data) => {
+    let ret = [];
+    data.forEach(el => {
+        ret.push(el.descriptions.map(desc => "· " + desc));
+    })
+    return ret;
+}
+
+const combineDegrees = (degrees) => {
+    let ret = "";
+    degrees.forEach((deg, ind) => {
+        ret += deg
+        if (ind !== degrees.length - 1) ret += ", "
+    })
+    return ret;
+}
+
+const combineYears = (years) => {
+    let ret = "";
+    years.forEach((deg, ind) => {
+        ret += deg
+        if (ind !== years.length - 1) ret += " / "
+    })
+    return ret;
+}
 
 const Resume = () => {
     return (
-        <div>
-            <p>
-                Edit <code>src/App.js</code> and save to reload RESUME page.
-            </p>
+        <div classname={styles.wrapper}>
+        <div className={styles.container}>
+            <div className={styles.category}>
+                <div className={styles.categoryName}><p>Experience</p></div>
+                <div className={styles.categoryContent}>
+                    {experience.map((data => (
+                        <>
+                            {data.length === 1 ?
+                                <ResumeSection title={data[0].title} subtitle={`${data[0].company} / ${data[0].start} - ${data[0].end}${notBlank(data[0].paren) ? " (" + data[0].paren + ")" : null}`} descriptions={data[0].descriptions.map(desc => "· " + desc)}/>
+                            : 
+                                <ResumeSection multi title={data[0].company} subtitles={getSubtitles(data)} descriptions={getDescriptions(data)}/>
+                            }
+                        </>
+                        
+                    )))}
+                </div>
+            </div> 
+            <div>
+                <div className={styles.category}>
+                    <div className={styles.categoryName}><p>Education</p></div>
+                    <div className={styles.categoryContent}>
+                        {resume.education.map((data => (
+                            <ResumeSection title={combineDegrees(data.degrees)} subtitle={data.school}/>
+                        )))}
+                    </div>
+                </div>
+                <div className={styles.category}>
+                    <div className={styles.categoryName}><p>Projects</p></div>
+                    <div className={styles.categoryContent}>
+                        {resume.projects.map((data => (
+                            <ResumeSection title={data.title + " / " + combineYears(data.years)} descriptions={[data.description]}/>
+                        )))}
+                    </div>
+                </div> 
+                <div className={styles.category}>
+                    <div className={styles.categoryName}><p>Skills</p></div>
+                    <div className={styles.categoryContent}>
+                        {Object.keys(resume.skills).map((key => (
+                            <ResumeSection title={key} descriptions={[resume.skills[key]]}/>
+                        )))}
+                    </div>
+                </div>
+                <div className={styles.category}>
+                    <div className={styles.categoryName}><p>Volunteer</p></div>
+                    <div className={styles.categoryContent}>
+                        {resume.volunteer.map((data => (
+                            <ResumeSection title={data.title} subtitle={`${data.company} / ${data.start} - ${data.end}${notBlank(data.paren) ? " (" + data.paren + ")" : null}`} descriptions={[data.description]}/>
+                        )))}
+                    </div>
+                </div>
+            </div>
+        </div>
         </div>
     );
 }
